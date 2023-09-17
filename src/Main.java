eb1fb0246ab34fcb3c0d1c061fca46d1fdda7feb
Aleksandr Anisimov <Kinvomarh87@gmail.com>
2023-09-17 17:23:18 +0300

    Changes Main

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите число: ");
+        System.out.println("Введите первое число: ");
         int firstNumber = new Scanner(System.in).nextInt();
-        System.out.println("Введите число: ");
+        System.out.println("Введите второе число: ");
         int secondNumber = new Scanner(System.in).nextInt();
         double quotient = (double) firstNumber/secondNumber;
-        System.out.println(firstNumber+secondNumber);
-        System.out.println(firstNumber-secondNumber);
-        System.out.println(firstNumber*secondNumber);
-        System.out.println(quotient);
+        System.out.println("Сумма: " + (firstNumber+secondNumber));
+        System.out.println("Разность: " + (firstNumber-secondNumber));
+        System.out.println("Произведение: " + (firstNumber*secondNumber));
+        System.out.println("Частное: " + quotient);
     }
 }
